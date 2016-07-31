cb9819ea3d8978de524897895a9f13972ee2cedb
gyeon <gyeon19@gmail.com>
2016-07-31 14:53:19 -0400

    add styling

--- app/models/app.js
+++ app/models/app.js
@@ -3,16 +3,16 @@ $(document).ready(() => {
   $(function(){
     $('#artist_form').on('submit',function(event){
       $('#articles').empty()
+      $('.image').empty()
       store.articles = []
       spotify_store.artist_albums = []
-      event.preventDefault()      
+      event.preventDefault()
       var artist = $('#artist').val()
-      var spotify = spotifyGetArtist(artist)        
-      var article = articleAdapter(artist).then(function appendData() { 
-          appendArticles() 
+      var spotify = spotifyGetArtist(artist)
+      var article = articleAdapter(artist).then(function appendData() {
+          appendArticles()
           appendAlbums()
         })
       })
     })
   })
-
