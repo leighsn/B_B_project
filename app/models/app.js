b610f43b34ef61e3babc78c35a2d7c1bf43ea46f
eaharonoff <emanuel.aharonoff@gmail.com>
2016-08-01 11:47:24 -0400

    bands-in-town api set-up

--- app/models/app.js
+++ app/models/app.js
@@ -1,23 +1,35 @@
 // kick off function on click
+// 247c8522b93a971f3eaa641d07aa8dc79210edc6
+// 6f4eef2c65b21ff642aa80ea898edd1c27c44983
 $(document).ready(() => {
-  $(function(){
+
     $('#artist_form').on('submit',function(event){
+      let artist = $('#artist').val()
       $('#articles').empty()
+      $('#artist_shows').remove()
+      $('#location-input').remove()
+      $('#location-submit').remove()
       $('.image').empty()
       $('.artist_name').empty()
       $('.albums').empty()
- // 
- // 247c8522b93a971f3eaa641d07aa8dc79210edc6
- // 6f4eef2c65b21ff642aa80ea898edd1c27c44983
+      $('#event_form').append(`<h3 id="artist_shows">${artist} Concerts by City:</h3><input id="location-input" type="text" ><input id="location-submit" type="submit">`)
       store.articles = []
       spotify_store.artist_albums = []
       event.preventDefault()
-      var artist = $('#artist').val()
       var spotify = spotifyGetArtist(artist)
       var article = articleAdapter(artist).then(function appendData() {
           appendArticles()
           appendAlbums()
-        })
-      })
-    })
-  })
+        })//return results from NYT API and append to HTML
+    })//actions kicked off by first submit button
+
+
+ $('body').on('submit', '#event_form', function(event){
+      event.preventDefault();
+      let location = $('#location-input').val()
+      let artist = $('#artist').val().replace((/[" "]/),"%20")
+      var eventFind = findEventNear(artist,location)
+
+  });//actions kicked off by second submit button 
+
+})
